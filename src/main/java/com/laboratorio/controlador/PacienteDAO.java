28cc5a1d82b7b5ef4b5471b46777bb615d976ddf
especialidades-medicas <especialidades.prolab@gmail.com>
2026-09-16 22:04:20 -0500

    Update PacienteDAO.java

--- src/main/java/com/laboratorio/controlador/PacienteDAO.java
+++ src/main/java/com/laboratorio/controlador/PacienteDAO.java
@@ -1,6 +1,10 @@
 package com.laboratorio.controlador;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
